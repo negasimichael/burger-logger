6e6e74ff511fc366a662f7b3e62d442277a86157
negasimichael <negasimichael1@gmail.com>
2021-02-14 00:49:21 -0600

    config/ file

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Set up MySQL connection.
+const mysql = require('mysql');
+
+const connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+
+	connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: "root",
+		password: '177471735@$nM',
+		database: 'burgers_db',
+	});
+}
+// Make connection.
+connection.connect(function (err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+// Export connection for our ORM to use.
+module.exports = connection;
