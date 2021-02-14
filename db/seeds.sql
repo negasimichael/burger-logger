36017ec35c85e429edd4c8651db518dc81c9ad0d
negasimichael <negasimichael1@gmail.com>
2021-02-14 00:53:31 -0600

    db/ scheam.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+-- Insert burgers
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE,CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE,CURRENT_TIMESTAMP);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Cheese Burger', FALSE,CURRENT_TIMESTAMP);
+
+SELECT * FROM burgers;
